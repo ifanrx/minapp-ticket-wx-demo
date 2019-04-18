91ef1450adcbb9c5e41f65941446437a237989c6
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-04-18 17:54:48 +0800

    组件内调用 ticket-report 时，开启 throttle

--- demo/app.js
+++ demo/app.js
@@ -3,7 +3,8 @@ App({
   onLaunch: function() {
     require('minapp-sdk')
     // wx.BaaS.auth.loginWithWechat()
+    // require('./sdk-wechat.2.0.8-a.js')
     wx.BaaS.login(false)
-    wx.BaaS.init(config.clientID)
+    wx.BaaS.init(config.clientID, {logLevel: 'debug', autoLogin: true})
   }
 })
